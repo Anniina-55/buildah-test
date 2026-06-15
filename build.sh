66b104b41f5125b264013832154a6955e1d7298c
Anniina-55 <anniina.leikas55@gmail.com>
2026-06-15 00:07:50 +0000

    Updated build script

--- build.sh
+++ build.sh
@@ -1,28 +1,31 @@
 #!/bin/bash
-
 set -e
 
 IMAGE_NAME="scratch-static-web"
 
-echo "Creating scratch container with Buildah..."
+echo "Creating scratch container..."
 
 ctr=$(buildah from scratch)
-
-echo "Mounting container filesystem..."
 mnt=$(buildah mount $ctr)
 
-echo "Creating web directory..."
+echo "Creating filesystem structure..."
 mkdir -p $mnt/www
+mkdir -p $mnt/bin
 
-echo "Copying files..."
+echo "Copying static files..."
 cp index.html $mnt/www/
 cp containers.png $mnt/www/
 
+echo "Adding busybox binary..."
+cp /bin/busybox $mnt/bin/
+
+echo "Setting permissions..."
+chmod +x $mnt/bin/busybox
+
 echo "Configuring container..."
-buildah config --cmd 'busybox httpd -f -p 8080 -h /www' $ctr
+buildah config --cmd "/bin/busybox httpd -f -p 8080 -h /www" $ctr
 
 echo "Committing image..."
 buildah commit $ctr $IMAGE_NAME
 
-echo "Done -> container image is committed!"
-buildah images
+echo "Done!"
